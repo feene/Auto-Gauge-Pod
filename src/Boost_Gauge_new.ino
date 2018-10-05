b457d4e746a966ac23398ca812b856a1a1b85f0c
feene <39316086+feene@users.noreply.github.com>
2018-10-04 22:40:01 -0600

    Moved to PlatformIO

--- src/Boost_Gauge_new.ino
+++ src/Boost_Gauge_new.ino
@@ -4,23 +4,23 @@ float boostVolt = 0;
 float boostKPa = 0;
 float boostPSI = 0;
 
-void setup() {  
-  
+void setup() {
+
   Serial.begin(9600);
-  Serial.println("Multigauge Start Up"); 
+  Serial.println("Multigauge Start Up");
 
 }
 
 void loop() {
-  
+
   boostVal = analogRead(boostPin);
   Serial.println("Boost Analog Value");
   Serial.println(boostVal);
-  
+
   boostVolt = boostVal * (5 / 1023.0);
   Serial.println("Boost Voltage");
   Serial.println(boostVolt);
-  
+
   boostKPa = (boostVal*(.00488)/(.022)+20);
   Serial.println("Boost KPa");
   Serial.println(boostKPa);
@@ -28,7 +28,7 @@ void loop() {
   boostPSI = (((boostVal/1023)+0.04)/0.004) * 0.145;
   Serial.println("Boost PSI");
   Serial.println(boostPSI);
-  
+
   delay(250);
 
 }
