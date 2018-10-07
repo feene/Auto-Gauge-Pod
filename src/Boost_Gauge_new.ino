09c22fa3c702ca6f610e82e83908894801e4f3b8
feene <39316086+feene@users.noreply.github.com>
2018-10-06 22:18:52 -0600

    Added correct code to refresh TFT Screen with Boost Value...

--- src/Boost_Gauge_new.ino
+++ src/Boost_Gauge_new.ino
@@ -3,6 +3,19 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include "Adafruit_ILI9341.h" // Hardware-specific library
+
+// TFT display and SD card will share the hardware SPI interface.
+// Hardware SPI pins are specific to the Arduino board type and
+// cannot be remapped to alternate pins.  For Arduino Uno,
+// Duemilanove, etc., pin 11 = MOSI, pin 12 = MISO, pin 13 = SCK.
+
+#define TFT_DC 9
+#define TFT_CS 10
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+// Variable Deffinitions
 
 int boostPin = A2;
 int boostVal = 0;
@@ -10,14 +23,11 @@ float boostVolt = 0;
 float boostKPa = 0;
 float boostPSI = 0;
 float atmBoostPSI = 0;
-
 float bmpTemp = 0;
 float bmpPressure = 0;
+bool debug_mode = false;
 
-
-bool debug_mode = true;
-
-Adafruit_BMP280 bmp; // I2C
+Adafruit_BMP280 bmp; // I2C interface for BMP280 --Also make sure the BMP280 Library is using the correct address 76 or 77
 
 
 void setup() {
@@ -31,6 +41,16 @@ void setup() {
     Serial.println("Could not find a valid BMP280 sensor, check wiring!");
     while (1);
   }
+
+   tft.begin();
+   delay(250);
+   tft.setRotation(1);
+   tft.fillScreen(ILI9341_BLUE);
+   tft.setCursor(30,30);
+   tft.setTextColor(ILI9341_WHITE);
+   tft.setTextSize(3);
+   tft.print("Boost: ");
+
 }
 
 void loop() {
@@ -45,11 +65,17 @@ void loop() {
   boostPSI = (((boostVal/1023)+0.04)/0.004) * 0.145;
 
   atmBoostPSI = ((boostKPa * 1000) - bmpPressure) * .000145038; // Convert MPX4250AP Readings with Atomospheric Reading to bind Reletive BOOST
+  tft.setCursor(150,30);
+  tft.setTextColor(ILI9341_WHITE,ILI9341_BLUE);
+  tft.print(atmBoostPSI);
+  delay(33);
+  tft.setTextColor(ILI9341_BLUE,ILI9341_BLUE);
+  tft.print(atmBoostPSI);
 
 // If Debug Mode is enabled text will be output to screen
 if ( debug_mode == true) {
-  Serial.println("**************************");
-  Serial.println("**************************");
+  Serial.println("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+  Serial.println("@@@@@@@@@@@@@@@@@@@@@@@@@@");
   Serial.println("Start of Boost Pressure Reading Output");
   Serial.println("Boost Analog Value");
   Serial.println(boostVal);
@@ -72,6 +98,6 @@ if ( debug_mode == true) {
   Serial.println(atmBoostPSI,1);
 
 }
-  delay(500);
+
 
 }
